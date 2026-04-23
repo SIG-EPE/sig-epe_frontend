f1cd2a6e2ced4295979a6501dd0291a8b99d2c61
Steve Castillo <stev8855@gmail.com>
2026-04-22 21:23:20 -0500

    chore(tests): add vitest/globals to tsconfig and update vitest setup path

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./src/test/setup.ts"],
+    setupFiles: ["./src/test-utils/setup.ts"],
     exclude: ["**/node_modules/**", "**/cypress/**"],
   },
   resolve: {
